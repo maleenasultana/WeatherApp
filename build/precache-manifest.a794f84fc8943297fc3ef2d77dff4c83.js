6702abe24dfd1ca391866417333480c9ec0d6ecb
Maleena <maleena.sohail@gmail.com>
2023-08-18 15:58:41 +0530

    ch

---
+++ build/precache-manifest.a794f84fc8943297fc3ef2d77dff4c83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28f63ec3ea8bde0af54e",
+    "url": "/static/css/main.8158178d.chunk.css"
+  },
+  {
+    "revision": "28f63ec3ea8bde0af54e",
+    "url": "/static/js/main.ef70f9f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a161d8cd4d3a5f68d7e",
+    "url": "/static/css/2.0aa480f2.chunk.css"
+  },
+  {
+    "revision": "6a161d8cd4d3a5f68d7e",
+    "url": "/static/js/2.0b3b11f7.chunk.js"
+  },
+  {
+    "revision": "d81f53b494aafddcfa147b72253cf134",
+    "url": "/index.html"
+  }
+];
